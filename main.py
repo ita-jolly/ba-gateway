c09a52b1fdf03fd8574d8c7363e416e3bdb112a4
Josef-TL <j.tonnesenlucas@gmail.com>
2024-12-06 13:47:07 +0100

    Remove db_service module and integrate SKADER service endpoints in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, request, jsonify
-import db_service
+import requests
+from flask import Flask, request, jsonify, make_response
 from flasgger import Swagger, swag_from
 from dotenv import load_dotenv
 from swagger.config import swagger_config
@@ -10,18 +10,33 @@ load_dotenv()
 app = Flask(__name__)
 swagger = Swagger(app, config=swagger_config)
 
-db_service.init()
+BILER_SERVICE_URL = os.getenv('BILER_SERVICE_URL')
+AFTALER_SERVICE_URL = os.getenv('AFTALER_SERVICE_URL')
+KUNDER_SERVICE_URL = os.getenv('KUNDER_SERVICE_URL')
+SKADER_SERVICE_URL = os.getenv('SKADER_SERVICE_URL')
 
 
 @app.route('/')
 def index():
   return "Welcome to API"
 
+@app.route('/skader', methods=['GET'])
+@swag_from('swagger/get_skader.yml')
+def get_skader():
+    response = requests.get(f"{SKADER_SERVICE_URL}/skader")
 
-# @app.route('/gettemplate', methods=['GET'])
-# @swag_from('swagger/get_template.yml')
-# def get_guests():
-#  return "got template"
+    return make_response(response.json(), response.status_code)
+
+
+@app.route('/skader', methods=['POST'])
+@swag_from('swagger/post_skader.yml')
+def create_skade():
+   data = request.json
+
+   response = requests.post(f"{SKADER_SERVICE_URL}/skader", json=data)
+
+   return make_response(response.json(), response.status_code)
+
 
 if __name__ == '__main__':
     app.run()
